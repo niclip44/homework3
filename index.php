727c83efecd96b066e773ad88c8aee4490129e8f
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-19 14:11:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,14 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Homework 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
 <table class="table table-striped">
   <thead>
     <tr>
-      <th>ID</th>
-      <th>Name</th>
+      <th>Courses</th>
     </tr>
   </thead>
   <tbody>
@@ -51,7 +50,7 @@ $conn->close();
   </tbody>
     </table>
     <h1>My Database is working!</h1>
-    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
+    <a class="btn btn-primary" href="database2.php" role="button">Table 2</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
