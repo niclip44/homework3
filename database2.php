74ea3c8e0534622ca8ba76aafd9440dc203653a0
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-19 14:26:20 -0500

    Update database2.php

--- database2.php
+++ database2.php
@@ -16,9 +16,9 @@
   <tbody>
     <?php
 $servername = "localhost";
-$username = "projecto_homework3";
-$password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
+$username = "makingmo_datauser6";
+$password = "X]kdLX*;]rEq";
+$dbname = "makingmo_Database1";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
