247eb262e88bb66be9e0ca68bcf1a892b8b244ed
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-21 13:12:58 -0500

    Update database4.php

--- database4.php
+++ database4.php
@@ -29,7 +29,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from Students order by Course_id";
+$sql = "SELECT * from Students";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
