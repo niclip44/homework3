3d492c8552b919c8f51ec789c48f03ab1ee61abd
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-21 12:57:28 -0500

    Update database2.php

--- database2.php
+++ database2.php
@@ -50,7 +50,7 @@ $conn->close();
     </table>
     <h1>Hello, world!</h1>
     <form action="handlepost.php" method="get">
-    <a class="btn btn-primary" href="index.php" role="button">Home</a>
+    <a class="btn btn-primary" href="index.php" role="button">Table 1</a>
       <a class="btn btn-primary" href="database3.php" role="button">Table 3</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
